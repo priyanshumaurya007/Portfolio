ca355bf209c722b5bf1794fe933c291273a5cd1a
Priyanshu <74262554+priyanshumaurya007@users.noreply.github.com>
2022-01-13 23:13:57 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/*  ============================================== typing animation ================================= */
-var typed = new Typed(".typing",{
-    strings:["Programmer","3 🌟 @CodeChef","Frontend developer","Data Science Enthusiast","Engineer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
