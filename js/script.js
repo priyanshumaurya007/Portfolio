7e4319fbd22db867b26a868fe3d9166907fae217
Priyanshu <74262554+priyanshumaurya007@users.noreply.github.com>
2022-01-14 16:53:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["Programmer","3 🌟 @CodeChef","Frontend developer","Data Science Enthusiast","Engineer",
     typeSpeed:100,
     BackSpeed:60,
     loop:true
